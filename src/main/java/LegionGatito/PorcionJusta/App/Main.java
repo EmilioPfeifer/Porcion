64ddcf6b95c1f9bc164c692e97c152a3dac94716
Pfeifer <emiliopfeifer@gmail.com>
2017-06-12 09:38:34 -0400

    actualizar

---
+++ src/main/java/LegionGatito/PorcionJusta/App/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LegionGatito.PorcionJusta.App;
+
+//import LegionGatito.PorcionJusta.GUI.VentanaInicio;
+import ventanas.VentanaInicio;
+
+/**
+ *
+ * @author UsuarioWin7
+ */
+public class Main {
+    public static void main(String[] args) {
+        VentanaInicio v = new VentanaInicio();
+    }
+}
