718a8b10dc524d419155ecdffa5d3ed903f54125
Pfeifer <emiliopfeifer@gmail.com>
2017-06-15 17:43:25 -0400

    revisar

--- src/main/java/ventanas/PanelCalculadoraComida.java
+++ src/main/java/ventanas/PanelCalculadoraComida.java
@@ -7,12 +7,14 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import modelo.TablaAlimento;
 
 public class PanelCalculadoraComida extends JPanel {
 	private JLabel textoIndicacion;
 	private JButton btnCalcular;
-	private JTextField cantidadCalorias;
-	private JLabel caloriasComida;
+	private JTextField txfGramosCalorias;
+	private JLabel lblCaloriasComida;
+        private TablaAlimento tablaAlimento;
 
 	
 	
@@ -25,29 +27,36 @@ public class PanelCalculadoraComida extends JPanel {
 		this.setLayout(null);
 
 		this.textoIndicacion = new JLabel("ingresar las calorias a consumir");
-		this.cantidadCalorias = new JTextField();
+		this.txfGramosCalorias = new JTextField();
 		this.btnCalcular = new JButton("Calcular");
-		this.caloriasComida = new JLabel("");
+		this.lblCaloriasComida = new JLabel("");
 
 		this.textoIndicacion.setBounds(50, 50, 200, 20);
-		this.cantidadCalorias.setBounds(100, 90, 80, 20);
+		this.txfGramosCalorias.setBounds(100, 90, 80, 20);
 		this.btnCalcular.setBounds(90, 130, 100, 20);
-		this.caloriasComida.setBounds(100, 170, 80, 20);
+		this.lblCaloriasComida.setBounds(100, 170, 80, 20);
 		
-		this.add(textoIndicacion,0);
-		this.add(cantidadCalorias,1);
-		this.add(btnCalcular,2);
-		this.add(caloriasComida,3);
+		this.add(textoIndicacion);
+                this.add(txfGramosCalorias);
+		this.add(btnCalcular);
+		this.add(lblCaloriasComida);
                  this.setBackground(new Color(137, 208, 171));
 		
 	}
+
+    public void cambiarLblCalorias(String gramos , String alimento){
+       String calorias =tablaAlimento.caloriasPorGramos(gramos, alimento);
+        this.lblCaloriasComida.setText(calorias);
+    }
+    public JLabel getLblCaloriasComida() {
+        return lblCaloriasComida;
+    }
 
     public JButton getBtnCalcular() {
         return btnCalcular;
     }
 
-    public JTextField getCantidadCalorias() {
-        return cantidadCalorias;
-    }
-        
+    public JTextField getTxfGramosCalorias() {
+        return txfGramosCalorias;
+    } 
 }
